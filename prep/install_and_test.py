29c3dad301f89a68252e9f10fbe3f4ed7c387dc4
AndersonDaniel <daniel.anderson.d@gmail.com>
2024-03-04 14:44:37 +0200

    pwl

--- prep/install_and_test.py
+++ prep/install_and_test.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     text_img = font.render("Environment is good!", True, pygame.Color("#7755ff"))
 
     text_x_center = 400
+    clock = pygame.time.Clock()
 
     while is_running:
         for event in pygame.event.get():
@@ -29,7 +30,8 @@ if __name__ == '__main__':
         window_surface.blit(background, (0, 0))
         window_surface.blit(text_img, (text_x_center - text_img.get_width() // 2, 80))
 
-        text_x_center = (text_x_center + 2 + text_img.get_width() // 2) % (800 + text_img.get_width()) \
+        text_x_center = (text_x_center + 5 + text_img.get_width() // 2) % (800 + text_img.get_width()) \
                         - text_img.get_width() // 2
 
         pygame.display.update()
+        clock.tick(60)
